bfb953a5abdd6f1fff95a2b1b05808a450b9b585
AndySchw <131532743+AndySchw@users.noreply.github.com>
2026-01-22 09:39:24 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -12,4 +12,21 @@ function subtract(a, b) {
 
 // TODO: Add multiply and divide functions
 
-module.exports = { add, subtract };
+function multiply(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    return a * b;
+}
+
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    if (b === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+}
+
+module.exports = { add, subtract, multiply, divide };
