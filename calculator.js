aa9eeb3e3faa2319412e9e5d3110e3d956bbcc69
AndySchw <131532743+AndySchw@users.noreply.github.com>
2026-01-22 09:39:11 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
 // Simple Calculator
 function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
     return a + b;
 }
 
